9eee2df5a0e8cba011f14dd98f89f247ef7a0063
peco <ricardocircle@icloud.com>
2020-08-13 14:18:21 +0800

    add components

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,11 @@
 <template>
-  <div id="app">
-  </div>
+  <div id="app"></div>
 </template>
 
 <script>
-
+import header from "./components/header/header";
+import goods from "./components/goods/goods";
 </script>
 
 <style>
-
 </style>
